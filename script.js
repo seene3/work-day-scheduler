22ffd4ab2283932b541a7a8fa71edf003aae1b21
Sean Gillespie <seene3@gmail.com>
2021-11-28 20:42:53 -0500

    added local storage

--- script.js
+++ script.js
@@ -5,15 +5,15 @@
 var currentDate = moment().format('ddd') + " " + moment().format("Do MMM YY")
 var hour = moment().hour();
 
-var nine = document.getElementById("#9am");
-var ten = document.getElementById("#10am");
-var eleven = document.getElementById("#11am");
-var twelve = document.getElementById("#12pm");
-var one = document.getElementById("#1pm");
-var two = document.getElementById("#2pm");
-var three = document.getElementById("#3pm");
-var four = document.getElementById("#4pm");
-var five = document.getElementById("#5pm");
+var nine = $("#9am");
+var ten = $("#10am");
+var eleven = $("#11am");
+var twelve = $("#12pm");
+var one = $("#1pm");
+var two = $("#2pm");
+var three = $("#3pm");
+var four = $("#4pm");
+var five = $("#5pm");
 
 console.log("the time is:", hour)
 console.log(currentDate)
@@ -24,7 +24,6 @@ var date = setInterval(function() {
 })
 
 function color() {
-
     $(".form-control").each(function () {
         var time = parseInt($(this).attr("id"))
         hour = parseInt(hour)
@@ -40,4 +39,42 @@ function color() {
     })
 }
 
+function storage() {
+    console.log("Current Hour " + hour);
+    var init9 = JSON.parse(localStorage.getItem("09:00am"));
+    nine.val(init9);
+  
+    var init10 = JSON.parse(localStorage.getItem("10:00am"))
+    ten.val(init10);
+    
+    var init11 = JSON.parse(localStorage.getItem("11:00am"))
+    eleven.val(init11);
+    
+    var init12 = JSON.parse(localStorage.getItem("12:00pm"))
+    twelve.val(init12);
+    
+    var init1 = JSON.parse(localStorage.getItem("01:00pm"))
+    one.val(init1);
+    
+    var init2 = JSON.parse(localStorage.getItem("02:00pm"))
+    two.val(init2);
+    
+    var init3 = JSON.parse(localStorage.getItem("03:00pm"))
+    three.val(init3);
+   
+    var init4 = JSON.parse(localStorage.getItem("04:00pm"))
+    four.val(init4);
+    
+    var init5 = JSON.parse(localStorage.getItem("05:00pm"))
+    five.val(init5);
+} 
+
+$(".saveBtn").on("click", function() {
+    userInput = $(this).siblings(".form-control").val();
+    hourSpan = $(this).siblings(".input-group-prepend").text();
+    localStorage.setItem(hourSpan, JSON.stringify(userInput));
+
+})
+
+storage();
 color();
